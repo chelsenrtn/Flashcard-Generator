221ccda314a4a6b1c664cd44c5bc2aacc1febdd0
chelsenrtn <chelsea.nrtn@gmail.com>
2017-04-15 10:54:13 -0700

    Add files via upload

---
+++ flashcards/questions.js
@@ -0,0 +1,74 @@
+exports.questions = [
+	{
+		full: 'What movie contains the line:"holde me like you did on lake Naboo"?',
+		cloze: 'Starwars Episode III: Revenge of the Sith.'
+	},
+	{
+		full: 'What counrty would you find the mouths of the Amizon river?',
+		cloze: 'Brazil'
+	},
+	{
+		full: 'What sort of animal is Segas Sonic?',
+		cloze: 'Hedgehog'
+	},
+	{
+		full: 'How many times are Mexican refried beans fried?',
+		cloze: 'Once'
+	},
+	{
+		full: 'Which famous poem contains the line "I am the master of my own fate, I am the captian of my soul".',
+		cloze: 'Invictus'
+	},
+	{
+		full: 'What is the first name of the real chef Boyardee?',
+		cloze: 'Hector'
+	},
+	{
+		full: 'What is the largest internal organ in the human body?',
+		cloze: 'The Liver'
+	},
+	{
+		full: 'What vitamnin is produced when a person is exposed to sunlight?',
+		cloze: 'D'
+	},
+	{
+		full: 'Who was the first U.S. predident to take the oath of office using a nickname?',
+		cloze: 'Jimmy Carter'
+	},
+	{
+		full: 'The Bubonic Plague was carried by fleas on rats aboard trading ships from the East and ravaged Europe for many centuries.',
+		cloze: 'Plague'
+	},
+	{
+		full: 'What spice is the most valuble in the Dune universe?',
+		cloze: 'Melange'
+	},
+	{
+		full: 'What year was the World Wide Web lauched in?',
+		cloze: '1991'
+	},
+	{
+		full: 'What are you if you are Myopic?',
+		cloze: 'Near-sighted'
+	},
+	{
+		full: 'What is the currency of Spain?',
+		cloze: 'The Euro'
+	},
+	{
+		full: 'What is the last name of the movie The Evil Deads protaganist Ash?',
+		cloze: 'Williams'
+	},
+	{
+		full: 'What is the spice Paprika made of?',
+		cloze: 'Bell pepper or chili pepper'
+	},
+	{
+		full: 'In what part of the body would you find the Cochlea?',
+		cloze: 'Ear'
+	},
+	{
+		full: 'What is the three term letter for a young goat?',
+		cloze: 'Kid'
+	}
+]
